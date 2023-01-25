122c5ff8bb3d4718669ee6480a1ba6891411dd5f
leejaeyeob <ljy11124@naver.com>
2023-01-25 21:51:13 +0900

    while & for how to using

---
+++ DAY 1/Section 07/Ex07-3-for-dict.py
@@ -0,0 +1,30 @@
+'''
+
+for문과 dict
+
+'''
+
+eng_dict = {
+    'sun':'태양',
+    'moon':'달',
+    'star':'별',
+    'space':'우주'
+}
+
+for word in eng_dict:
+    print('{}의 뜻 : {}'.format(word, eng_dict.get(word)))
+
+
+eng_dict_kyes = eng_dict.keys()
+for key in eng_dict_kyes:
+    print('eng_dict의 keys {}'.format(key))
+
+
+
+
+
+
+
+
+
+
