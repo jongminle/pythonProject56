19aead68e8b00eb2f3b9277f124d8155815ae035
leejaeyeob <ljy11124@naver.com>
2023-01-30 21:45:31 +0900

    day06

---
+++ Day06/Section12/Ex12-10-external.py
@@ -0,0 +1,51 @@
+'''
+
+패키지
+    모듈 상위 개념
+    모듈의 집합을 의미한다.
+
+패키시 설치
+console> pip install package 명
+
+패키지 삭제
+console> pip uninstall package 명
+
+'''
+
+# 행열 연산 관련 package
+import numpy as np
+
+print(np.sum([1,2,3,4,5]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
