347e27b6141f3101c677b75bb7c8064b3b318957
leejaeyeob <ljy11124@naver.com>
2023-01-30 21:34:50 +0900

    day06

---
+++ Day06/Section12/Ex12-3-module3.py
@@ -0,0 +1,34 @@
+'''
+
+ * 사용하기
+
+'''
+
+
+from converter import *
+
+miles = kilometer_to_miles(150)
+print('150km = {}miles'.format(miles))
+
+pound = gram_to_pounds(1000)
+print('1000g = {}pounds'.format(pound))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day06/Section12/Ex12-4-module4.py
@@ -0,0 +1,27 @@
+'''
+
+별명 사용하기 1
+
+'''
+
+
+import converter as cvt
+
+miles = cvt.kilometer_to_miles(145)
+print('150km = {}miles'.format(miles))
+
+pounds = cvt.gram_to_pounds(1000)
+print('1000g = {}pounds'.format(pounds))
+
+
+
+
+
+
+
+
+
+
+
+
+
