369c2d2f34ceb56e5e93f1c96cb4bd5b01f9ef0a
leejaeyeob <ljy11124@naver.com>
2023-02-06 20:00:21 +0900

    23/02/06 -1

--- Day08/Section14/Ex14-3-carWriter.py
+++ Day08/Section14/Ex14-3-carWriter.py
@@ -12,7 +12,7 @@ with open('차량관리.csv', 'w', encoding='UTF-8') as file:
     csv_maker.writerow([1, '25다1234', '2020-10-20,14:10'])
     csv_maker.writerow([1, '28하1234', '2020-10-20,14:20'])
 print('차량관리.csv 파일이 생성되었습니다.')
-#Dd
+
 
 
 
