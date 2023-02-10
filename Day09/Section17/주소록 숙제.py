726daed167e821d7e6f78d8fec4f12c5e34a7e25
leejaeyeob <ljy11124@naver.com>
2023-02-10 21:26:23 +0900

    23/02/10

---
+++ Day09/Section17/주소록 숙제.py
@@ -0,0 +1,35 @@
+print()
+print('메뉴 리스트')
+
+print()
+print('1. 신규 주소록 등록')
+print('2. 기존 수소록 삭제')
+print('3. 기존 주소록 수정')
+print('4. 주소록 검색')
+print('5. 전체 주소록 출력')
+print('0. 프로그램 종료')
+print()
+
+selct=int(input('수행할 작업을 숫자로 입력하세요 >>>'))
+
+if selct==1:
+    add_name=input('이름을 입력하세요 >>>')
+    add_PHnum=input('연락처를 입력하세요 >>>')
+    add_add=input('주소를 입력하세요 >>>')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
