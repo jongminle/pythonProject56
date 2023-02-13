e3db7f99f6d67a1d0b29e0707bcee885dc01d0d6
leejaeyeob <ljy11124@naver.com>
2023-02-13 21:48:04 +0900

    23/02/13

--- Day09/Section18/Ex18-6-getRankPage2.py
+++ Day09/Section18/Ex18-6-getRankPage2.py
@@ -8,7 +8,8 @@ response = requests.get(url)
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
-movie_list = soup.find_all('div', class_='tit3')
+#movie_list = soup.find_all('div', class_='tit3')
+movie_list = soup.find_all('div', attrs={'class': 'tit3'})  #(이것도 가능)
 
 for idx, movie in enumerate(movie_list):
     print('{}위 : {}'.format(idx + 1, movie.text.strip()))
